5c099baa72c350a60f45ce310427b9449e53138d
Mosaabk <mosaabk@gmail.com>
2019-04-16 14:35:47 -0500

    Decorate My Text - Lab 6

--- js/Lab8.js
+++ js/Lab8.js
@@ -5,9 +5,45 @@ String.prototype.filter = function (word) {
 
 Array.prototype.bubbleSort = function(){
 
+    let a = this;
+    var swapped;
+    do {
+        swapped = false;
+        for (var i=0; i < a.length-1; i++) {
+            if (a[i] > a[i+1]) {
+                var temp = a[i];
+                a[i] = a[i+1];
+                a[i+1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+
+    return a;
 }
 
-var str = "hello";
+let Person = function(){
+     this.name = "default";
+    this.teach = function (subject) {
+        console.log(this.name + " is now teaching " + subject);
+    }
+};
+
+
+const person = {
+    name : "default",
+    teach : function (subject) {
+        console.log(this.name + " is now teaching " + subject);
+    }
+};
 
 console.log("This house is not nice!".filter("not "));
+console.log([6,4,0, 3,-2,1].bubbleSort());
 
+let teacher = new Person();
+teacher.name = "Tina";
+teacher.teach("WAP");
+
+let teacher2 = Object.create(person);
+teacher2.name = "Asaad";
+teacher2.teach("MWA");
--- js/test.js
+++ js/test.js
@@ -1,276 +1,90 @@
-// $(document).ready(function(){
-//
-//    console.log( $("li").css("color",
-//         function(idx, oldValue) {
-//             switch (idx) {
-//                 case 0:
-//                     return "green";
-//                 case 1:
-//                     return "yellow";
-//                 case 2:
-//                     return "red";
-//             }
-//         }));
-//
-//    let newLi = $("<li>",{
-//        id : "new",
-//        text : "Hello from JS",
-//        class : "Special",
-//        css : {
-//            backgroundColor : "blue",
-//            color : "red",
-//        },
-//        click : function () {
-//            $(this).css("color", "white")
-//        }
-//
-//
-//    });
-//
-//    $("ul").append(newLi);
-//    // $("li:contains('Hello')").remove();
-//
-// });
-
-// function sam() {
-//     this.newvar = "hello";
-// }
-// // console.log(newvar); // Uncaught ReferenceError:
-// sam(); // this = window
-// console.log("newvar: " + newvar); //hello
-//
-// const b = {
-//     log: function() {
-//         console.log("this: ");
-//         console.log(this);
+// x = 1;
+// var a = 5;
+// var b = 10;
+// var c = function(a, b, c) {
+//     console.log("x1: "+ x);
+//     console.log("a2: "+ a);
+//     var f = function() {
+//         b = a;
+//         console.log("b3: "+ b);
+//         b = c;
+//         var a = 3;
 //     }
+//     f();
+//     console.log("b4:" + b);
+//     x = 6;
 // }
-//
-// console.log(b);
-// b.log();
-
-//
-// window.onload = function () {
-//     document.getElementById("btn1").onclick = function () {
-//         console.log(this);
-//         this.style.color = "red";
-//     }
-// }
-
-// var a = {
-//     name: "",
-//     log: function() {
-//
-//         let self = this;
-//
-//         self.name = "Hello";
-//         console.log(this.name); //Hello
-//         var setFrench = newName =>{ this.name = newName};
-//         setFrench("Bonjour");
-//         console.log(this.name); //Hello
-//     }
-// };
-//
-// a.log();
-
-// var me = {
-//     first: 'Josh',
-//     last: 'Splinter',
-//     getFullName: function () {
-//         return this.first + ' ' + this.last;
-//     }
-// };
-// var you = {
-//     first: 'William',
-//     last: 'Smith'
-// };
-// console.log(me.getFullName.apply(you));
-// console.log(me.getFullName.bind(you)());
-
-
-// var funcs = [];
-//
-// for(var i =0; i < 5; i++){
-//     funcs[i] = (function(n){
-//         return function () {
-//             return n;
-//         };
-//     })(i)
-// }
-//
-// console.log(funcs[0]());
-// console.log(funcs[1]());
-// console.log(funcs[2]());
-// console.log(funcs[3]());
-// console.log(funcs[4]());
-
-
-// const Module = (function(){
-//
-//     const privateMethod = function(message){
-//         console.log(message);
-//     };
-//
-//
-//     const publicMethod = function(text){
-//         privateMethod(text);
-//     };
-//
-//
-//     return {
-//         publicMethod : publicMethod
-//     };
-//
-// })();
-//
-// Module.extension = function(){
-//     console.log('hello from extension');
-// }
-// Module.publicMethod("hello");
-//
-// Module.extension();
-
-
-//
-// const counter = function(){
-//
-//     let privateCounter = 0;
-//
-//     const changeBy = function(val){
-//         privateCounter += val;
-//     }
-//
-//     return {
-//         increment : function () {
-//             changeBy(1);
-//         },
-//         decrement: function () {
-//             changeBy(-1);
-//         },
-//         value : function () {
-//             return privateCounter;
-//         }
-//     }
-//
-//
-// };
-//
-//
-// const counter1 = counter();
-// const counter2 = counter();
-//
-// console.log(counter1.value());
-// counter1.increment();
-// console.log(counter1.value());
-// counter2.decrement();
-//
-// console.log(counter2.value());
-
-
-// const a ={}
-//
-// console.log(a.__proto__);
-//
-// const b = function () {
-//
-// }
-//
-// console.log(b.__proto__)
-// console.log(b.__proto__.__proto__);
-//
-// const x  = [];
-//
-// console.log(x.__proto__);
-
-
-// const person = {
-//
-//     firstName : "John",
-//     lastName  : "Doe",
-//     greet      : function () {
-//         return "Hi, " + this.firstName +" " + this.lastName;
-//     }
-// }
-//
-//
-// const p1 = Object.create(person);
-//
-//
-// p1.firstName = "Musab";
-//
-//
-//
-// console.log(p1.__proto__);
-
-//
-// const Person = function(){
-//     console.log(this);
-//
-//     this.university = "MUM";
-//     year =  2019;
-// }
-//
-// const faculty = new Person();
-//
-// Person.prototype.greet = function () {
-//     return "Hi from " + this.university;
-// }
-//
-// console.log(faculty.greet());
-
-//
-// function Course(coursename) {
-//
-//     this.coursename = coursename;
-//
-//     console.log("Function constructor invoked");
-//
-// }
-//
-// Course.prototype.register = function () {
-//     return "Register " + this.coursename;
-// }
-//
-// const wap = new Course("WAP");
-//
-// console.log(wap);
-// console.log(wap.__proto__);
-// console.log(wap instanceof Course); // true
-// console.log(wap.register());
-
-
+// c(8, 9, 7);
+// console.log("b5: " + b);
+// console.log("x6: "+ x);
 // $(function(){
 //
-//     $("#textbox").mouseout(sayHi);
+//     // $("input[type='submit']").click(function(e){
+//     //     e.preventDefault();
+//     //
+//     //     const seats = $("input[name='seat']").val();
+//     //     const zipCode
+//     //     console.log((seats));
+//     //
+//     //     if(seats > 200){
+//     //         $("div").append("<p> you can't book more than 200 seats");
+//     //     }
+//     // });
 //
-//     $("#submit").click(sayHi);
 //
-//     $("input[type='radio']").click(alertme);
+//     $("form").submit(function (e) {
+//
+//         e.preventDefault();
+//         let submitFlag = true;
+//
+//         const  seats = $("input[name='seat']");
+//         const zipCode = $("input[name='zipcode']");
+//         const street = $("input[name='street'");
+//
+//         if (street === ''){
+//             $("div").append("<p>you must enter a street name.</p>");
+//             submitFlag=false;
+//         }
+//
+//         if(seats < 0 || seats > 200){
+//             $("div").append("<p>you can't book more than 200 seats</p>");
+//             submitFlag=false;
+//         }
+//
+//         if(zipCode === ''){
+//             $("div").append("<p>You must enter a Zip code. </p>");
+//             submitFlag= false;
+//         }
+//
+//         if($("input[type='checkbox']:checked").val() === undefined){
+//             $("div").append("<p>you must select one extras</p>");
+//             submitFlag = false;
+//         }
+//
+//         if($("input[type='radio']:checked").val() === undefined){
+//             $("div").append("<p>you must select type of taxi</p>");
+//             submitFlag = false;
+//
+//         }
+//
+//         if(submitFlag){
+//             this.submit();
+//         }
+//     })
+//
 // });
-//
-// function sayHi(){
-//     return this.value = "Hi";
-// }
-//
-// function alertme(){
-//     return alert(this.value);
-// }
 
 
-const Person = {
-    name : "unkown",
-    age : 20
+var x = 9;
+
+function myFunction(){
+
+    return x * x;
 }
 
-const  John = Object.create(Person);
+document.write(myFunction());
 
-console.log(John.name);
+x = 5;
 
-Person.changeName = function(newName){
-    console.log(this);
-    this.name = newName;
-}
-John.name = "musab";
+document.write(myFunction());
 
-John.changeName("Eric");
-console.log(John.name);
